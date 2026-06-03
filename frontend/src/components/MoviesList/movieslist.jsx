ae29f938cd9837977c0a5c72a85b11797ed20969
Lapalus Brune <brune.lapalus@student-cs.fr>
2026-06-03 17:04:56 +0200

    clickmovie details

--- frontend/src/components/MoviesList/movieslist.jsx
+++ frontend/src/components/MoviesList/movieslist.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import './movieslist.css'; // Importation du fichier CSS
+import { Link } from 'react-router-dom';
+import './movieslist.css';
 
-function movieslist({ movies, apiError }) {
+function MoviesList({ movies, apiError }) {
   return (
     <div className="movies-list">
       {movies.length > 0
         ? movies.map((movie) => (
-            <div key={movie.id} className="movie-card">
-              {movie.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                  alt={movie.title}
-                  className="movie-poster"
-                />
-              ) : (
-                <div className="no-image">Pas d'image</div>
-              )}
-              <h3 className="movie-title">{movie.title}</h3>
-              <p className="movie-rating">Note : {movie.vote_average}/10</p>
-            </div>
+            <Link
+              to={`/movie/${movie.id}`}
+              key={movie.id}
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              <div className="movie-card">
+                {movie.poster_path ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                    className="movie-poster"
+                  />
+                ) : (
+                  <div className="no-image">Pas d'image</div>
+                )}
+                <h3 className="movie-title">{movie.title}</h3>
+                <p className="movie-rating">Note : {movie.vote_average}/10</p>
+              </div>
+            </Link>
           ))
         : !apiError && <p className="loading-text">Chargement des films...</p>}
     </div>
   );
 }
 
-export default movieslist;
+export default MoviesList;
